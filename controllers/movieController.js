78733f22ff401e572d05579216c523699aa87545
tega <alessiotegoni4@gmail.com>
2025-01-10 13:24:45 +0100

    add: movie img, avg votes

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,10 +1,33 @@
 import asyncHandler from "express-async-handler";
 import db from "../db/conn.js";
+import { getImageUrl } from "../lib/utils.js";
 
 export const getAllMovies = asyncHandler(async (req, res) => {
-  const query = "SELECT * FROM movies";
-  const [results] = await db.query(query);
-  res.status(200).json(results);
+  const { search } = req.query;
+
+  let sql = "SELECT * FROM movies";
+
+  if (search)
+    sql += ` WHERE title LIKE %${search}% OR abstract LIKE %${search}%`;
+
+  const [results] = await db.query(sql);
+
+  let movies = results.map(async (movie) => {
+    const [[{ avgVote }]] = await db.query(
+      "SELECT ROUND(AVG(vote), 2) AS avgVote FROM reviews WHERE movie_id = ?",
+      [movie.id]
+    );
+
+    return {
+      ...movie,
+      image: getImageUrl(movie.image),
+      avgVote,
+    };
+  });
+
+  movies = await Promise.all(movies);
+
+  res.status(200).json(movies);
 });
 
 export const getMovieById = asyncHandler(async (req, res) => {
@@ -16,12 +39,22 @@ export const getMovieById = asyncHandler(async (req, res) => {
   const reviewsQuery = db.query("SELECT * FROM reviews WHERE movie_id = ?", [
     movieId,
   ]);
+  const avgVoteQuery = db.query(
+    "SELECT AVG(vote) AS avgVote FROM reviews WHERE movie_id = ?",
+    [movieId]
+  );
 
-  const [[[movie]], [reviews]] = await Promise.all([movieQuery, reviewsQuery]);
+  const [[[movie]], [reviews], [[{ avgVote }]]] = await Promise.all([
+    movieQuery,
+    reviewsQuery,
+    avgVoteQuery,
+  ]);
 
   if (!movie) {
     res.status(404);
     throw new Error("Movie not found");
   }
-  res.status(200).json({ ...movie, reviews });
+  res
+    .status(200)
+    .json({ ...movie, image: getImageUrl(movie.image), reviews, avgVote });
 });
